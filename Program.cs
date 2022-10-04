24b7216bafe2dfff51570697a8b5ebbd6d929459
Fabricio <fabriciodev@outlook.com>
2022-10-04 09:58:33 -0300

    Entregando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Modelo 8", imei: "d55d45dd5", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "12345", modelo: "Modelo 8", imei: "d55d45dd5", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("DIO");
